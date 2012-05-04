ac6a77948d3f4a9b9bd315eb12eeb3bdf35eb66c
Tyler Leonhardt <bubbleking2@gmail.com>
2012-05-04 11:34:38 -0400

    Attempts to place traffic light

--- Test Logger/Test Logger Pro Source Code/Test Logger Pro/mainForm.cs
+++ Test Logger/Test Logger Pro Source Code/Test Logger Pro/mainForm.cs
@@ -11,58 +11,41 @@ namespace Test_Logger_Pro
 {
     public partial class mainForm : Form
     {
-        Dictionary<int, string> instructions;
-        bool instructionsFileValid = true;
+        Dictionary<int, string> events;
+        bool eventFileValid = true;
 
-        Dictionary<int, string> parseInstructionFile()
+        /// <summary>
+        /// Reads the "Event List.txt" file into a dictionary.
+        /// </summary>
+        /// <returns>The dictionary filled with events and their call values.</returns>
+        Dictionary<int, string> loadEventFile()
         {
-            Dictionary<int, string> instructions = new Dictionary<int,string>();
-            string[] instructionsList = System.IO.File.ReadAllLines("Event List.txt");
+            Dictionary<int, string> events = new Dictionary<int,string>();
+            string[] eventList = System.IO.File.ReadAllLines("Event List.txt");
 
-            for(int i = 0; i < instructionsList.Length; i++)
+            for(int i = 3; i < eventList.Length - 1; i++)
             {
-                string[] thisInstruction = instructionsList[i].Split('|');
-                if (thisInstruction.Length == 5)
-                {
-                    for (int j = 0; j < thisInstruction[1].Length; j++)
-                    {
-                        if (thisInstruction[1][j] < 48 || thisInstruction[1][j] > 57)
-                        {
-                            thisInstruction[1] = thisInstruction[1].Remove(j, 1);
-                            j--;
-                        }
-                    }
-                    for (int j = 0; j < thisInstruction[2].Length; j++)
-                    {
-                        if (thisInstruction[2][j] == ' ')
-                        {
-                            thisInstruction[2] = thisInstruction[2].Remove(j, 1);
-                            j--;
-                        }
-                    }
-                    for (int j = 0; j < thisInstruction[3].Length; j++)
-                    {
-                        if (thisInstruction[3][j] == ' ')
-                        {
-                            thisInstruction[3] = thisInstruction[3].Remove(j, 1);
-                            j--;
-                        }
-                    }
+                string[] thisEvent = eventList[i].Split('|');
 
-                    try
-                    {
-                        instructions.Add(Convert.ToInt32(thisInstruction[1]), thisInstruction[2] + ' ' + thisInstruction[3]);
-                    }
-                    catch
-                    {
-                        instructionsFileValid = false;
-                        break;
-                    }
+                try
+                {
+                    events.Add(Convert.ToInt32(thisEvent[1].Replace(" ", "")), thisEvent[2].Replace(" ", ""));
+                }
+                catch
+                {
+                    eventFileValid = false;
+                    break;
                 }
             }
-            return instructions;
+
+            return events;
         } 
 
+        /// <summary>
+        /// Converts testLog arrays into C# int arrays
+        /// </summary>
+        /// <param name="input">The testLog array being input.</param>
+        /// <returns>The int array being output.</returns>
         int[] dump(string input)
         {
             string[] data = input.Split(',');
@@ -85,12 +68,18 @@ namespace Test_Logger_Pro
                 }
                 catch
                 {
+
                 }
             }
 
             return output;
         }
 
+        /// <summary>
+        /// Reads the integers in an input array and converts them into string instructions.
+        /// </summary>
+        /// <param name="input">The integer array containing instructions.</param>
+        /// <returns>The string instructions to output.</returns>
         string[] intToInstruction(int[] input)
         {
             string[] commands = new string[input.Length];
@@ -99,9 +88,20 @@ namespace Test_Logger_Pro
 
             for (int i = 0; i < input.Length; i++)
             {
-                if (instructions.ContainsKey(input[i]))
+                int firstTwoDigits = input[i];
+                while(firstTwoDigits >= 100)
+                    firstTwoDigits /= 10;
+
+                if (events.ContainsKey(firstTwoDigits))
                 {
-                    commands[i] = instructions[input[i]];
+                    if (input[i] % 10 == 1)
+                        commands[i] = "Begin ";
+                    else if (input[i] % 10 == 2)
+                        commands[i] = "Kill ";
+                    else
+                        commands[i] = "End ";
+
+                    commands[i] += events[firstTwoDigits];
                 }
                 else
                 {
@@ -120,6 +120,11 @@ namespace Test_Logger_Pro
                 return output;
         }
 
+        /// <summary>
+        /// Adds tabs to a string array of events to format it for output.
+        /// </summary>
+        /// <param name="input">The string array being converted.</param>
+        /// <returns>The array containing the correctly formatted data.</returns>
         string[] prepareStringForOutput(string[] input)
         {
             int tabCount = 0;
@@ -132,6 +137,7 @@ namespace Test_Logger_Pro
                 else
                     tabCount--;
             }
+
             if (tabCount < 0)
                 tabCount = 0;
 
@@ -139,10 +145,27 @@ namespace Test_Logger_Pro
             {
                 if (input[i][0] == 'E')
                     tabCount--;
+                if (input[i][0] == 'K')
+                {
+                    for (int j = i - 1; input[j] != "Begin " + input[i].Split()[1] && j >= 0; j--)
+                    {
+                        if(input[j][0] == 'B')
+                            tabCount--;
+                        if (input[j][0] == 'E')
+                            tabCount++;
+                    }
+                    
+                    tabCount--;
+                    if(tabCount < 0)
+                        tabCount = 0;
+                }
+
                 string thisOutput = i + "\t";
+
                 for (int j = 0; j < tabCount; j++)
                     thisOutput += '\t';
                 output[i] += thisOutput + input[i];
+
                 if (input[i][0] == 'B')
                     tabCount++;
             }
@@ -154,9 +177,9 @@ namespace Test_Logger_Pro
         {
             InitializeComponent();
             System.IO.File.AppendAllText("Event List.txt", "");
-            instructions = parseInstructionFile();
+            events = loadEventFile();
             saveFile.InitialDirectory = System.IO.Directory.GetCurrentDirectory();
-            if (!instructionsFileValid)
+            if (!eventFileValid)
             {
                 outputBox.Items.Add("Invalid Instructions File");
                 windowDumpButton.Enabled = false;
@@ -167,6 +190,7 @@ namespace Test_Logger_Pro
 
         private void fileDumpButton_Click(object sender, EventArgs e)
         {
+            events = loadEventFile();
             int[] data = dump(inputBox.Text);
             string[] instructions = intToInstruction(data);
             string[] output = prepareStringForOutput(instructions);
@@ -177,6 +201,7 @@ namespace Test_Logger_Pro
 
         private void windowDumpButton_Click(object sender, EventArgs e)
         {
+            events = loadEventFile();
             outputBox.Items.Clear();
             int[] data = dump(inputBox.Text);
             string[] instructions = intToInstruction(data);
